90f97e93c2024a8dba61e065fecbb8611d402506
LeonardoT-V <iderltv12@gmail.com>
2024-06-13 00:39:17 -0500

    feature: add other format to date

--- app/lib/date.ts
+++ app/lib/date.ts
@@ -7,3 +7,13 @@ export function FormatToDateIntl(date: string | Date): string {
   }).format(parseDate);
   return formatedDate
 }
+
+export function FormatToDateSmall(date: string | Date): string {
+  const parseDate = new Date(date);
+  const formatedDate = new Intl.DateTimeFormat("es-EC", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  }).format(parseDate);
+  return formatedDate
+}
