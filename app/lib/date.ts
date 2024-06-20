863ac8919087cc104218be995201c295a528326d
LeonardoT-V <iderltv12@gmail.com>
2024-06-19 23:18:27 -0500

    feature: new date formater with include hour and minutes

--- app/lib/date.ts
+++ app/lib/date.ts
@@ -17,3 +17,15 @@ export function FormatToDateSmall(date: string | Date): string {
   }).format(parseDate);
   return formatedDate
 }
+
+export function FormatToDateWithHour(date: string | Date): string {
+  const parseDate = new Date(date);
+  const formatedDate = new Intl.DateTimeFormat("es-EC", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: 'numeric',
+    minute: 'numeric'
+  }).format(parseDate);
+  return formatedDate
+}
